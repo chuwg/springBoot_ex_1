64e5138cdbba6564597404c08e386a6374b8f4d6
chuwg <gi828282@naver.com>
2022-01-20 15:08:24 +0900

    gitignore

---
+++ src/main/java/com/roobistory/tistory/service/posts/PostsService.java
@@ -0,0 +1,48 @@
+package com.roobistory.tistory.service.posts;
+
+import com.roobistory.tistory.domain.posts.Posts;
+import com.roobistory.tistory.domain.posts.PostsRepository;
+import com.roobistory.tistory.web.dto.PostsListResponseDto;
+import com.roobistory.tistory.web.dto.PostsResponseDto;
+import com.roobistory.tistory.web.dto.PostsSaveRequestDto;
+import com.roobistory.tistory.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+    public PostsResponseDto findById (Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new
+                        IllegalArgumentException("해당 게시글이 없습니다. id=" +id));
+        return new PostsResponseDto(entity);
+    }
+
+    @Transactional
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/com/roobistory/tistory/web/PostsApiController.java
@@ -0,0 +1,31 @@
+package com.roobistory.tistory.web;
+
+import com.roobistory.tistory.service.posts.PostsService;
+import com.roobistory.tistory.web.dto.PostsResponseDto;
+import com.roobistory.tistory.web.dto.PostsSaveRequestDto;
+import com.roobistory.tistory.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+
+public class PostsApiController {
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id)
+    {
+        return postsService.findById(id);
+    }
+}
