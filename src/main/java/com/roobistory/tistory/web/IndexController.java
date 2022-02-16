b4f91f1608ed87ed4ec8e298d534ca35f62c902e
chuwg <gi828282@naver.com>
2022-02-16 10:15:01 +0900

    gitignore

--- src/main/java/com/roobistory/tistory/web/IndexController.java
+++ src/main/java/com/roobistory/tistory/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.roobistory.tistory.web;
 
+import com.roobistory.tistory.config.auth.LoginUser;
 import com.roobistory.tistory.config.auth.dto.SessionUser;
 import com.roobistory.tistory.service.posts.PostsService;
 import com.roobistory.tistory.web.dto.PostsResponseDto;
@@ -19,9 +20,9 @@ public class IndexController {
     private  final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
