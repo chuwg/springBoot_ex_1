623bd71d27a58fde116cae4d3f3ac5ebba72c3d6
chuwg <gi828282@naver.com>
2022-04-05 15:49:34 +0900

    before deploy edit 2

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,13 +7,14 @@ echo "> Bulid 파일 복사"
 
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-echo "> 현재 구동 중인 애플리케이션 pid 확인"
+echo "> 현재 구동 중인 애플리케이션 pid 확인1"
 
 CURRENT_PID=$(pgrep -fl chuwg-springboot-webservice | grep jar | awk '{print $1')
 
 echo "현재 구동 중인 애플리케이션 pid : $CURRENT_PID"
 
-if [ -z "CURRENT_PID" ]; then
+# shellcheck disable=SC2157
+if [ -z "$CURRENT_PID" ]; then
   echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> kill -15 $CURRENT_PID"
