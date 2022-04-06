15abd4f52ca6d081aacbfee43b018a369606c933
chuwg <gi828282@naver.com>
2022-04-06 11:06:42 +0900

    똑같은 상황이라 다시 변경

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,11 +8,10 @@ echo "> Bulid 파일 복사"
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
-# CURRENT_PID=$(pgrep -f ${PROJECT_NAME}.*.jar)
-CURRENT_PID=$(pgrep -f springBoot_ex_1.*.jar)
+CURRENT_PID=$(pgrep -f ${PROJECT_NAME}.*.jar)
+
 echo "현재 구동 중인 애플리케이션 pid : $CURRENT_PID"
 
-# shellcheck disable=SC2157
 if [ -z "$CURRENT_PID" ]; then
   echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
